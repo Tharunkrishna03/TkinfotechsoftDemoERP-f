b446722e6e8f0a1696e3ab3676e74b8ebdbe3d8b
THARUN KRISHNA <dtharunkrishna65@gmail.com>
2026-06-25 10:43:58 +0530

    Fix staticfiles error by adding STATIC_ROOT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"] if (BASE_DIR / "static").exists() else []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
